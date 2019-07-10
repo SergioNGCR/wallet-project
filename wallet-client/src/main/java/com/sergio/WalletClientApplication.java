bec8d096d03c5b5433fd5aa80ba44aa082b3e9d5
SergioNGCR <sergio-n-g@hotmail.com>
2019-07-09 23:40:44 -0600

    Adding initial projects

--- wallet-client/src/main/java/com/sergio/WalletClientApplication.java
+++ wallet-client/src/main/java/com/sergio/WalletClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WalletServerApplication {
+public class WalletClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WalletServerApplication.class, args);
+        SpringApplication.run(WalletClientApplication.class, args);
     }
 
 }
--- wallet-client/src/test/java/com/sergio/WalletClientApplicationTests.java
+++ wallet-client/src/test/java/com/sergio/WalletClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WalletServerApplicationTests {
+public class WalletClientApplicationTests {
 
     @Test
     public void contextLoads() {
